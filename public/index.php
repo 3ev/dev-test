cf4e4717a2d8e8e0060f0eff010079978d03889d
David McNair <dave-mcnair@hotmail.com>
2019-03-14 15:25:28 +0000

    issue2 show user-friendly messages on Guzzle exceptions

--- public/index.php
+++ public/index.php
@@ -7,11 +7,19 @@ $twig = new Twig_Environment($loader, ['debug' => true]);
 
 //Get the episodes from the API
 $client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
-
+try {
+    $error = null;
+    $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
+    $data = json_decode($res->getBody(), true);
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    $data = [];
+    $error = 'Sorry, there was an error. Please try again by reloading the page.';
+}
 //Sort the episodes
 array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
 
 //Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+echo $twig->render('page.html', [
+    "episodes" => $data,
+    "error" => $error
+]);
