540e07a6a769c9e4fb52e9402c11d32d50cec6d0
David McNair <dave-mcnair@hotmail.com>
2019-03-14 14:48:34 +0000

    issue3 show only one episode popover at a time

--- public/index.php
+++ public/index.php
@@ -1,16 +1,39 @@
 <?php
+
+use GuzzleHttp\HandlerStack;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+use Kevinrob\GuzzleCache\Storage\FlysystemStorage;
+use League\Flysystem\Adapter\Local;
+
 require_once '../vendor/autoload.php';
 
 //Load Twig templating environment
 $loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader, ['debug' => true]);
 
-//Get the episodes from the API
-$client = new GuzzleHttp\Client();
+// Create default HandlerStack
+$handlerStack = HandlerStack::create();
+$handlerStack->push(
+    new CacheMiddleware(
+        new GreedyCacheStrategy(
+            new FlysystemStorage(
+                new Local('../cache')
+            ),
+            5 //ttl in seconds
+        )
+    ),
+    'greedy-cache'
+);
+$client = new GuzzleHttp\Client([
+    'base_uri' => 'http://3ev.org',
+    'handler' => $handlerStack
+]);
 try {
     $error = null;
-    $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-    $data = json_decode($res->getBody(), true);
+    //Get the episodes from the API
+    $response = $client->request('GET', 'dev-test-api');
+    $data = json_decode($response->getBody(), true);
 } catch (\GuzzleHttp\Exception\GuzzleException $e) {
     $data = [];
     $error = 'Sorry, there was an error. Please try again by reloading the page.';
