f65ceabb0d7aa891bc7760b8c8f42110d57244df
ar487 <akamerus@gmail.com>
2019-04-05 13:59:59 +0100

    fixed issue2 - Display error message when API is not available

--- public/index.php
+++ public/index.php
@@ -7,15 +7,26 @@ $twig = new Twig_Environment($loader, ['debug' => true]);
 
 //Get the episodes from the API
 $client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
 
-// Get values of season and episode in two seprate arrays which will be used for sorting
-$season  = array_column($data, 'season');
-$episode = array_column($data, 'episode');
+// Catch errors in connections to the API
+try{
+    $res = $client->request('GET', 'http://3ev.org/dev-test-api/');
+    
+    $data = json_decode($res->getBody(), true);
 
-//Sort the data multi-array accroding to season and episode in ascending order respectively
-array_multisort($season, SORT_ASC, $episode, SORT_ASC, $data);
- 
-//Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+    // Get values of season and episode in two seprate arrays which will be used for sorting
+    $season  = array_column($data, 'season');
+    $episode = array_column($data, 'episode');
+
+    //Sort the data multi-array accroding to season and episode in ascending order respectively
+    array_multisort($season, SORT_ASC, $episode, SORT_ASC, $data);
+    
+    //Render the template
+    echo $twig->render('page.html', ["episodes" => $data]);
+
+}catch (\Exception $e){
+    $error = 'Sorry, there was an error. Please try again by reloading the page.';
+    //Render the template
+    echo $twig->render('page.html', ["error" => $error]);
+}
+
